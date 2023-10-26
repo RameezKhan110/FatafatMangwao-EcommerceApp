e7c7b2142d4e9b359b3f8d85a499a9177bdd14fa
Rameez Khan <rameez.khan.ahmed1100@gmail.com>
2023-10-26 10:06:29 +0500

    Designed 25 screens

--- app/src/main/java/com/example/fatafatmangwao/UserSpecificProductFragment.kt
+++ app/src/main/java/com/example/fatafatmangwao/UserSpecificProductFragment.kt
@@ -13,10 +13,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [UserSpecificProduct.newInstance] factory method to
+ * Use the [UserSpecificProductFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class UserSpecificProduct : Fragment() {
+class UserSpecificProductFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -44,12 +44,12 @@ class UserSpecificProduct : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment UserSpecificProduct.
+         * @return A new instance of fragment UserSpecificProductFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            UserSpecificProduct().apply {
+            UserSpecificProductFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
